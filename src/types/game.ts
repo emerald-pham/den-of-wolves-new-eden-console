44e99fefe5c818b96b63e2e1a45cde82054be5cf
Emerald Pham <10377738+emerald-pham@users.noreply.github.com>
2026-09-22 09:28:34 -0400

    fix: hydrate environmental maintenance results

--- src/types/game.ts
+++ src/types/game.ts
@@ -1269,7 +1269,7 @@ export const MAINTENANCE_EVENT_ACTIONS = [
   'begin', 'storage', 'rations', 'unrest', 'riot', 'reactor', 'production', 'bays', 'end', 'additional-labour',
 ] as const;
 export type MaintenanceEventAction = typeof MAINTENANCE_EVENT_ACTIONS[number];
-export const MAINTENANCE_EVENT_RESULT_STEPS = ['1', '2', '3', '4', '5', '6', '7'] as const;
+export const MAINTENANCE_EVENT_RESULT_STEPS = ['0', '1', '2', '3', '4', '5', '6', '7'] as const;
 export type MaintenanceEventResultStep = typeof MAINTENANCE_EVENT_RESULT_STEPS[number];
 export type MaintenanceEventResults = Partial<Record<MaintenanceEventResultStep, string>>;
 
